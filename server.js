3f07bccfa71c2ebf9e4327fdf6ce13776337bd24
stevemurphy256 <smurphy5884@icloud.com>
2017-10-05 20:07:48 -0400

    heroku setup

--- server.js
+++ server.js
@@ -19,12 +19,18 @@ app.set("view engine", "handlebars");
 var mysql = require("mysql");
 
 //Needs password
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "burger_db"
-});
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
